b77603923868fd92aa592c8a29f1ef385d05e795
snee <snee.andy@gmail.com>
2023-06-10 18:28:21 -0400

    Automated templating of project carousel images

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
 from flask import Flask, render_template, request, url_for, redirect
-from email.mime.text import MIMEText
-import smtplib
-from email.message import EmailMessage
+import os
+import re
+import glob
+from pathlib import Path
 app = Flask(__name__)
 
 
+def slugify(s):
+    s = s.lower().strip()
+    s = re.sub(r'[^\w\s-]', '', s)
+    s = re.sub(r'[\s_-]+', '-', s)
+    s = re.sub(r'^-+|-+$', '', s)
+    return s
+
+
 class Project:
-    def __init__(self, name, link, coverimage, ):
-        self.name = name
-        self.link = link
-        self.coverimage = coverimage
+    def __init__(self, title, project_id):
+        self.title = title
+        self.__create_link()
+        self.project_id = project_id
+        self.static_path = "/static/projects/{}".format(project_id)
+        self.cover_image = self.static_path + "/cover.jpg"
+
+    def __create_link(self):
+        self.link = "/" + slugify(self.title)
+
+    def image_paths(self):
+        jpg_images = glob.glob1(os.getcwd() + self.static_path, "*.jpg")
+        png_images = glob.glob1(os.getcwd() + self.static_path, "*.png")
+        print(jpg_images + png_images)
+        all_images = jpg_images + png_images
+        all_images.remove("cover.jpg")
+        return all_images
 
 
-yaejiFG = Project("Yaeji - For Granted", "#", "/static/images/for_granted_cover.jpg")
-boogie = Project("Big Room Boogie", "#", "/static/images/boogie_cover.jpg")
-project_list = [yaejiFG, boogie]
+
+yaejiFG = Project("Yaeji - For Granted", "for_granted")
+boogie = Project("Big Room Boogie", "big_room_boogie")
+yaejiOneMore = Project("Yaeji One More Tour", "one_more")
+project_list = [yaejiFG, boogie, yaejiOneMore]
 
 
 @app.route("/")
@@ -22,15 +46,15 @@ def index():
     return render_template("homepage.html", projects=project_list)
 
 
-@app.route("/<project_name>")
-def project_view(project_name):
-    print("project name")
-    print(str(project_name))
+@app.route("/<project_slug>")
+def project_view(project_slug):
     for project in project_list:
-        print(project.name)
-        if str(project_name) == project.name:
-            return render_template("project_page.html", target_project=project)
+        if str(project_slug) in project.link:
+            return render_template("project_page.html", target_project=project, projects=project_list)
 
+@app.route("/contact")
+def contact_view():
+    return render_template("contact_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
