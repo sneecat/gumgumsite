568ffaf5531c9b807c5adddeec37fd0243140793
snee <snee.andy@gmail.com>
2023-06-10 14:07:16 -0400

    init

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, url_for, redirect
+from email.mime.text import MIMEText
+import smtplib
+from email.message import EmailMessage
+app = Flask(__name__)
+
+
+class Project:
+    def __init__(self, name, link, coverimage):
+        self.name = name
+        self.link = link
+        self.coverimage = coverimage
+
+
+yaejiFG = Project("Yaeji - For Granted", "#", "/static/images/for_granted_cover.jpg")
+boogie = Project("boogie", "#", "/static/images/boogie_cover.jpg")
+project_list = [yaejiFG, boogie]
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", projects=project_list)
+
+
+@app.route("/sendmail/", methods=["POST"])
+def sendemail():
+    if request.method == "POST":
+        name = request.form["name"]
+        subject = request.form["Subject"]
+        email = request.form['_replyto']
+        message = request.form['message']
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
