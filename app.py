83277a2d3154863d76f1efa2b91b53cdfb1f5593
snee <snee.andy@gmail.com>
2023-06-10 15:58:32 -0400

    create image gallery hovertext

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 
 class Project:
-    def __init__(self, name, link, coverimage):
+    def __init__(self, name, link, coverimage, ):
         self.name = name
         self.link = link
         self.coverimage = coverimage
 
 
 yaejiFG = Project("Yaeji - For Granted", "#", "/static/images/for_granted_cover.jpg")
-boogie = Project("boogie", "#", "/static/images/boogie_cover.jpg")
+boogie = Project("Big Room Boogie", "#", "/static/images/boogie_cover.jpg")
 project_list = [yaejiFG, boogie]
 
 
@@ -22,13 +22,14 @@ def index():
     return render_template("homepage.html", projects=project_list)
 
 
-@app.route("/sendmail/", methods=["POST"])
-def sendemail():
-    if request.method == "POST":
-        name = request.form["name"]
-        subject = request.form["Subject"]
-        email = request.form['_replyto']
-        message = request.form['message']
+@app.route("/<project_name>")
+def project_view(project_name):
+    print("project name")
+    print(str(project_name))
+    for project in project_list:
+        print(project.name)
+        if str(project_name) == project.name:
+            return render_template("project_page.html", target_project=project)
 
 
 if __name__ == "__main__":
